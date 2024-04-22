fe3058bd2705d0f2a8164132c136fe01b1ae10f5
ouweiya <higogle1@gmail.com>
2024-04-22 14:52:11 +0800

    Updates

---
+++ component---src-pages-404-js-62ed56fde0c4fd65dfe6.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkjotai_website=self.webpackChunkjotai_website||[]).push([[883],{429:function(e,t,a){a.r(t),a.d(t,{default:function(){return i}});var c=a(7294),h=a(1562);function i(){return(0,c.useEffect)((()=>{(0,h.c4)("/")}),[]),null}globalThis.jotaiAtomCache=globalThis.jotaiAtomCache||{cache:new Map,get(e,t){return this.cache.has(e)?this.cache.get(e):(this.cache.set(e,t),t)}}}}]);
+//# sourceMappingURL=component---src-pages-404-js-62ed56fde0c4fd65dfe6.js.map
