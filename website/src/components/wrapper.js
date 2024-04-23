af0843edd42ffba21701db5815b42f47a0aa7cf4
ouweiya <higogle1@gmail.com>
2024-04-23 14:01:16 +0800

    Update sidebar and wrapper components with responsive design changes

--- website/src/components/wrapper.js
+++ website/src/components/wrapper.js
@@ -1,7 +1,7 @@
 export const Wrapper = ({ children, ...rest }) => {
   return (
     <div
-      className="relative flex flex-col lg:mx-auto lg:max-w-[1920px] lg:flex-row lg:justify-around"
+      className="relative flex flex-col lg:mx-auto lg:max-w-[1920px] lg:flex-row lg:justify-start lg:gap-16 lg:ml-[140px]"
       {...rest}
     >
       {children}
