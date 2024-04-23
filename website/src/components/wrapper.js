00cfea3148f8efea42f947c87ad5e1f9d6eb0e6c
ouweiya <higogle1@gmail.com>
2024-04-23 15:25:33 +0800

    Update wrapper component with responsive design changes

--- website/src/components/wrapper.js
+++ website/src/components/wrapper.js
@@ -1,7 +1,7 @@
 export const Wrapper = ({ children, ...rest }) => {
   return (
     <div
-      className="relative flex flex-col lg:mx-auto lg:max-w-[1920px] lg:flex-row lg:justify-start lg:gap-16 lg:ml-[140px]"
+      className="relative flex flex-col lg:mx-auto lg:max-w-[1920px] lg:flex-row lg:justify-start 2xl:gap-16 lg:pl-36"
       {...rest}
     >
       {children}
