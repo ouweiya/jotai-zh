95634c1fd7f125cf16b8dc8c64ce02b03b070cc6
ouweiya <higogle1@gmail.com>
2024-04-23 15:36:01 +0800

    Update wrapper component with adjusted gap in responsive design

--- website/src/components/wrapper.js
+++ website/src/components/wrapper.js
@@ -1,7 +1,7 @@
 export const Wrapper = ({ children, ...rest }) => {
   return (
     <div
-      className="relative flex flex-col lg:mx-auto lg:max-w-[1920px] lg:flex-row lg:justify-start 2xl:gap-16 lg:pl-36"
+      className="relative flex flex-col lg:mx-auto lg:max-w-[1920px] lg:flex-row lg:justify-start 2xl:gap-20 lg:pl-36"
       {...rest}
     >
       {children}
